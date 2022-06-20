f0c30a3d21f0e68dc8c192c9355945fad1115c42
Bogumił Laska <bogumil_laska@epam.com>
2022-06-20 23:52:24 +0200

    Solution

--- cache.go
+++ cache.go
@@ -3,21 +3,47 @@ package cache
 import "time"
 
 type Cache struct {
+	store map[string]Entry
+}
+
+type Entry struct {
+	value    string
+	deadline time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{store: make(map[string]Entry)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func NewEntry(newValue string) Entry {
+	return Entry{value: newValue, deadline: time.Now().Add(time.Duration(time.Duration.Hours(72)))}
 }
 
-func (receiver) Put(key, value string) {
+func (cache Cache) Get(key string) (string, bool) {
+	entry := cache.store[key]
+	if entry.value == "" {
+		return "", false
+	}
+
+	if entry.deadline.After(time.Now()) {
+		return "", false
+	}
+
+	return entry.value, true
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Put(key, value string) {
+	cache.store[key] = NewEntry(value)
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache Cache) Keys() []string {
+	keys := make([]string, 0, len(cache.store))
+	for k := range cache.store {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.store[key] = Entry{value: value, deadline: deadline}
 }
